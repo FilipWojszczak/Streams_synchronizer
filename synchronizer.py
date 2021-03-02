ca43dc6d8ac02080c2289c4240552842b391e8c2
FilipWojszczak <filip.wojszczak@gmail.com>
2021-03-02 21:13:17 +0100

    Add multiqueue element to pipeline

--- synchronizer.py
+++ synchronizer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import gi
+import time
 
 gi.require_version("GLib", "2.0")
 gi.require_version("GObject", "2.0")
@@ -14,12 +15,11 @@ Gst.init_check(None)
 class Scheduler:
     def __init__(self, uri_list):
         self.uri_list = uri_list
-        self.videos = [Video(uri) for uri in self.uri_list]
+        self.videos = Videos(self.uri_list)
         self.gui = GUI(self.videos)
 
     def start_videos_buffering(self):
-        for video in self.videos:
-            video.pause()
+        self.videos.play()
 
     def start_working(self):
         self.gui.window.show_all()
@@ -35,22 +35,42 @@ class Scheduler:
         pass
 
 
-class Video:
-    def __init__(self, uri):
+class Videos:
+    def __init__(self, uri_list):
         # Link: "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm"
-
-        self.source = Gst.ElementFactory.make("uridecodebin", "source")
-        self.source.set_property('uri', uri)
-        self.vconvert = Gst.ElementFactory.make("videoconvert", "vconvert")
-        self.gtksink = Gst.ElementFactory.make("gtksink", "sink")
+        self.uri_list = uri_list
         self.pipeline = Gst.Pipeline().new()
+        self.sources = []
+        self.vconverts = []
+        self.gtksinks = []
+        self.multiqueue = Gst.ElementFactory.make("multiqueue", "multiqueue")
+        self.pipeline.add(self.multiqueue)
+        self.multiqueue.set_property('sync-by-running-time', True)
+        self.multiqueue.connect("pad-added", self.on_multiqueue_pad_added)
 
-        self.pipeline.add(self.source)
-        self.pipeline.add(self.vconvert)
-        self.pipeline.add(self.gtksink)
+        for index, uri in enumerate(self.uri_list):
+            source_string = "source" + str(index + 1)
+            vconvert_string = "vconvert" + str(index + 1)
+            gtksink_string = "sink" + str(index + 1)
+            self.sources.append(Gst.ElementFactory.make("uridecodebin", source_string))
+            self.vconverts.append(Gst.ElementFactory.make("videoconvert", vconvert_string))
+            self.gtksinks.append(Gst.ElementFactory.make("gtksink", gtksink_string))
 
-        self.vconvert.link(self.gtksink)
-        self.source.connect("pad-added", self.on_pad_added)
+            self.pipeline.add(self.sources[index])
+            self.pipeline.add(self.vconverts[index])
+            self.pipeline.add(self.gtksinks[index])
+
+        for index, uri in enumerate(self.uri_list):
+            self.sources[index].connect("pad-added", self.on_source_pad_added)
+            self.sources[index].set_property('uri', uri)
+
+            vconvert_pad = self.vconverts[index].get_static_pad("src")
+            multiqueue_pad = self.multiqueue.request_pad(self.multiqueue.get_pad_template("sink_%u"), None, None)
+            vconvert_pad.link(multiqueue_pad)
+
+        # self.multiqueue.set_property('max-size-bytes', 0)
+        # self.multiqueue.set_property('max-size-buffers', 0)
+        # self.multiqueue.set_property('max-size-time', 0)
 
     def play(self):
         self.pipeline.set_state(Gst.State.PLAYING)
@@ -61,12 +81,27 @@ class Video:
     def stop(self):
         self.pipeline.set_state(Gst.State.READY)
 
-    def on_pad_added(self, src, new_pad):
-        sink_pad = self.vconvert.get_static_pad("sink")
-        new_pad.link(sink_pad)
+    def on_source_pad_added(self, src, new_pad):
+        for index, source in enumerate(self.sources):
+            for i in range(new_pad.get_current_caps().get_size()):
+                print(index, "***", new_pad.get_current_caps().get_structure(i))
+            if source == src:
+                sink_pad = self.vconverts[index].get_static_pad("sink")
+                if not sink_pad.is_linked():
+                    new_pad.link(sink_pad)
+                    print("link")
+                    break
+
+    def on_multiqueue_pad_added(self, src, new_pad):
+        for gtksink in self.gtksinks:
+            sink_pad = gtksink.get_static_pad("sink")
+            if not sink_pad.is_linked():
+                new_pad.link(sink_pad)
+                break
 
     def show_widget(self):
-        self.gtksink.props.widget.show()
+        for gtksink in self.gtksinks:
+            gtksink.props.widget.show()
 
 
 class GUI:
@@ -106,9 +141,9 @@ class GUI:
         self.main_grid.attach_next_to(self.slider_box, self.videos_box, Gtk.PositionType.BOTTOM, 1, 1)
         self.main_grid.attach_next_to(self.buttons_box, self.slider_box, Gtk.PositionType.BOTTOM, 1, 1)
 
-        for video in videos:
-            self.videos_box.pack_start(video.gtksink.props.widget, True, True, 0)
-            video.show_widget()
+        for gtksink in videos.gtksinks:
+            self.videos_box.pack_start(gtksink.props.widget, True, True, 0)
+        videos.show_widget()
 
         self.slider_box.pack_start(self.slider, True, True, 0)
 
@@ -117,20 +152,24 @@ class GUI:
         self.buttons_box.pack_start(self.stop_button, True, True, 0)
 
     def on_play(self, button, videos):
-        for video in videos:
-            video.play()
+        videos.play()
 
     def on_pause(self, button, videos):
-        for video in videos:
-            video.pause()
+        videos.pause()
 
     def on_stop(self, button, videos):
-        for video in videos:
-            video.stop()
+        videos.stop()
+        # print(str(videos[0].pipeline.get_clock().get_time()) + "\n" +
+        #       str(videos[1].pipeline.get_clock().get_time()) + "\n*****\n" +
+        #       str(videos[0].pipeline.query_position(Gst.Format.TIME)) + "\n" +
+        #       str(videos[1].pipeline.query_position(Gst.Format.TIME)) + "\n*****\n" +
+        #       str(videos[0].pipeline.get_base_time()) + "\n" +
+        #       str(videos[1].pipeline.get_base_time()) + "\n")
 
 
-links = ["https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm",
-         "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm"]
+links = ["http://192.168.0.100:8080/video", "http://192.168.0.100:8080/video"]
+# links = ["https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm",
+#          "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm"]
 scheduler = Scheduler(links)
-scheduler.start_videos_buffering()
+# scheduler.start_videos_buffering()
 scheduler.start_working()
