4e377e9004305cb14b9b0332ae3313aaedcc2290
FilipWojszczak <filip.wojszczak@gmail.com>
2021-03-26 14:22:33 +0100

    Implement buttons in GUI, OpenCV sync

--- opencv-synchronizer.py
+++ opencv-synchronizer.py
@@ -11,44 +11,59 @@ from PIL import Image, ImageTk
 class Scheduler:
     def __init__(self, source_list, max_bfr_size, min_start_bfr_size):
         self.gui = GUI(len(source_list))
-        self.min_start_bfr_size = min_start_bfr_size
+        self.gui_state = "STOP"
+        self.ready = False
         self.videos = [Video(source, max_bfr_size, min_start_bfr_size) for source in source_list]
-        self.playing_state = False
+        self.check_state()
+        self.min_start_bfr_size = min_start_bfr_size
         self.capture_errors = [0 for source in source_list]
 
+    def check_state(self):
+        for video in self.videos:
+            video.state = self.gui.state
+        if self.gui_state != "PLAY" and self.gui.state == "PLAY":
+            self.gui_state = self.gui.state
+            self.play_all()
+        else:
+            self.gui_state = self.gui.state
+        self.gui.window.after(1, self.check_state)
+
     def play(self, index):
-        if self.playing_state or self.check_all_buffers():
-            self.playing_state = True
+        if self.ready or self.check_all_buffers():
+            # if index == 0:
+            #     print(self.videos[index].get_frames_list_size())
+            self.ready = True
             try:
                 frame_to_display = self.videos[index].get_frame()
                 self.check_buffer_difference(index)
-                # if index == 0:
-                #     print(len(self.videos[index].frames_list))
-                frame_to_display = cv2.cvtColor(frame_to_display,
-                                                cv2.COLOR_BGR2RGB)  # OpenCV treat images as having BGR layers, Pillow as RGB
-                img = Image.fromarray(frame_to_display)
-                # img = self.adjust_frame_size(img, img.size)
-                img = img.resize((320, 240))
-                imgtk = ImageTk.PhotoImage(image=img)
-                self.gui.add_frame(index, imgtk)
+                img_tk = self.process_image(frame_to_display)
+                self.gui.add_frame(index, img_tk)
+
                 self.capture_errors[index] = 0
-                # cv2.imshow("window" + str(i), frame_to_display)
             except IndexError:
                 self.capture_errors[index] += 1
                 if self.capture_errors[index] >= 50:
-                    self.playing_state = False
-        self.gui.videos_area[0].after(10, self.play, index)
+                    self.ready = False
+
+        if self.gui_state == "PLAY":
+            self.gui.videos_area[0].after(10, self.play, index)
 
     def play_all(self):
         for i in range(len(self.videos)):
             self.play(i)
+
+    def process_image(self, image):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # OpenCV treat images as having BGR layers, Pillow as RGB
+        image = Image.fromarray(image)
+        # image = self.adjust_frame_size(image, image.size)
+        image = image.resize((320, 240))
+        return ImageTk.PhotoImage(image=image)
 
     def check_buffer_difference(self, index):
         min_list = min([v.get_frames_list_size() for v in self.videos])
         diff = self.videos[index].get_frames_list_size() - min_list
         if diff > 30:
             self.videos[index].delete_frames(diff - 1)
-            print("difference", index)
 
     def check_all_buffers(self):
         result = True
@@ -79,6 +94,7 @@ class Video(cv2.VideoCapture):
         super().__init__(*args, **kwargs)
         self.name = name
         self.cap = cv2.VideoCapture(self.name)
+        self.state = "STOP"
         self.frames_list = []
         self.thread = threading.Thread(target=self._capture, args=(max_bfr_frms,))
         self.thread.daemon = True
@@ -96,12 +112,15 @@ class Video(cv2.VideoCapture):
                 # self.cap.release()
                 self.cap = cv2.VideoCapture(self.name)
             else:
-                if len(self.frames_list) > max_buffer_frames:
+                if (len(self.frames_list) > max_buffer_frames and self.state == "PLAY") or self.state == "STOP":
                     try:
                         self.frames_list.pop(0)
                     except IndexError:
                         pass
-                self.frames_list.append(frame)
+                if self.state != "STOP":
+                    self.frames_list.append(frame)
+                else:
+                    self.frames_list.clear()
 
     def get_frame(self):
         return self.frames_list.pop(0)
@@ -116,6 +135,7 @@ class Video(cv2.VideoCapture):
 class GUI:
     def __init__(self, cameras_number):
         self.default_size = (320, 240)
+        self.state = "STOP"
         self.window = tk.Tk()
         self.window.wm_title("Synchronizer")
 
@@ -140,16 +160,6 @@ class GUI:
         for i, area in enumerate(self.videos_area):
             self.add_frame(i, image)
 
-        # self.videos_frame = tk.Frame(self.window)
-        # self.videos_frame.pack(expand=tk.YES)
-        # # self.videos_frame.grid(row=0, column=0)
-        #
-        # self.videos_area = []
-        # for row in range(((cameras_number - 1) // 3) + 1):
-        #     for column in range(3):
-        #         self.videos_area.append(tk.Label(self.videos_frame))
-        #         self.videos_area[-1].grid(row=row, column=column, padx=2, pady=2)
-
         self.buttons_frame = tk.Frame(self.window, pady=5)
         self.buttons_frame.pack(fill=tk.BOTH)
         self.play_button = tk.Button(self.buttons_frame, text="Play", command=self.on_play)
@@ -170,30 +180,29 @@ class GUI:
         #     self.add_frame(i, image)
 
     def on_play(self):
-        print(self.videos_area[0].winfo_width(), self.videos_area[0].winfo_height())
-        print(self.videos_area[3].winfo_width(), self.videos_area[3].winfo_height())
+        self.state = "PLAY"
+        # print(self.videos_area[0].winfo_width(), self.videos_area[0].winfo_height())
+        # print(self.videos_area[3].winfo_width(), self.videos_area[3].winfo_height())
         # print(self.buttons_frame.winfo_width(), self.buttons_frame.winfo_height())
 
     def on_pause(self):
-        pass
+        self.state = "PAUSE"
 
     def on_stop(self):
-        pass
+        self.state = "STOP"
 
-    def add_frame(self, index, imgtk):
-        self.videos_area[index].imgtk = imgtk
-        self.videos_area[index].configure(image=imgtk)
+    def add_frame(self, index, img_tk):
+        self.videos_area[index].imgtk = img_tk
+        self.videos_area[index].configure(image=img_tk)
 
 
-# cap = Video("http://192.168.0.100:8080/video", 10)
 cameras = [
     # "http://192.168.0.101:8080/video",
     # "rtsp://192.168.0.104:8080/h264_pcm.sdp",
     # "rtsp://192.168.0.102:8080/h264_pcm.sdp",
     "http://192.168.0.102:8080/video",
-    "http://192.168.0.104:8080/video",
+    # "http://192.168.0.104:8080/video",
     0
 ]
 scheduler = Scheduler(cameras, 100, 50)
-scheduler.play_all()
 scheduler.display_GUI()
